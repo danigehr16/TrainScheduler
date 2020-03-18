c59ec3a175a7a8150be878f697a8acf08fc33381
dani <gehr.danielle@gmail.com>
2020-03-18 17:10:57 -0400

    added html cells to type in and some css design

---
+++ assets/Javascript/train.js
@@ -0,0 +1,6 @@
+var database = firebase.database();
+
+// connect firebase database
+// create places for users to type to get information
+// grab train data from database
+// create a place to store the data from database
